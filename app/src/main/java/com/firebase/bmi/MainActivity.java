8c168a6b42f6fd78aca6ce00ea741ecedafc8967
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2019-11-05 18:34:29 +0100

    Improved UI, added spinners, firebase, design and images

--- app/src/main/java/com/firebase/bmi/MainActivity.java
+++ app/src/main/java/com/firebase/bmi/MainActivity.java
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private EditText height;
-    private EditText weight;
-    private TextView result;
+public class MainActivity extends AppCompatActivity {
     private EditText name;
     private EditText gender;
+    private String weightSpinnerValue,heightSpinnerValue,genderSpinnerValue;
+    Spinner customSpinnerW,customSpinnerH,customSpinnerG;
+
+    List<String> weightList;
+    List<String> heightList;
+    List<String> genderList;
 
 
     @Override
@@ -22,20 +30,105 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         name = findViewById(R.id.nameEditText);
-        gender = findViewById(R.id.genderEditText);
-        height = findViewById(R.id.heightEditText);
-        weight = findViewById(R.id.weightEditText);
-        result = findViewById(R.id.resultButton);
+
+        /**SPINNERS // WEIGHT //HEIGHT //GENDER
+        /**List Ready For Spinner/Drop Down**/
+        prepareWeightList();
+        prepareHeightList();
+        prepareGenderList();
+
+        /**Custom Spinner for Weight**/
+        //spinner for weight
+        customSpinnerW = findViewById(R.id.customSpinnerWeight);
+        ArrayAdapter<String> customSpinnerWeight = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, weightList);
+        customSpinnerWeight.setDropDownViewResource(R.layout.custom_dropdown);
+        customSpinnerW.setAdapter(customSpinnerWeight);
+        customSpinnerW.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //store weight value for calcBMI
+                weightSpinnerValue = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }});
+
+        /**Custom Spinner for Height**/
+        //spinner for height
+        customSpinnerH = findViewById(R.id.customSpinnerHeight);
+        ArrayAdapter<String> customSpinnerHeight = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, heightList);
+        customSpinnerHeight.setDropDownViewResource(R.layout.custom_dropdown);
+        customSpinnerH.setAdapter(customSpinnerHeight);
+        customSpinnerH.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //store height value for calcBMI
+                heightSpinnerValue = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }});
+
+        /**Custom Spinner for Gender**/
+        //spinner for gender
+
+        customSpinnerG = findViewById(R.id.customSpinnerGender);
+        ArrayAdapter<String> customSpinnerGender = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, genderList);
+        customSpinnerGender.setDropDownViewResource(R.layout.custom_dropdown);
+        customSpinnerG.setAdapter(customSpinnerGender);
+        customSpinnerG.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //store height value for calcBMI
+                genderSpinnerValue = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }});
+
+
+}
+
+
+    private void prepareWeightList() {
+        weightList = new ArrayList<>();
+        /**Add Items in Spinner List**/
+        weightList.add("72");
+        weightList.add("80");
+
     }
 
+    private void prepareHeightList() {
+        heightList = new ArrayList<>();
+        /**Add Items in Spinner List**/
+        heightList.add("160");
+        heightList.add("182");
+
+    }
+
+    private void prepareGenderList() {
+        genderList = new ArrayList<>();
+        /**Add Items in Spinner List**/
+        genderList.add("Male");
+        genderList.add("Female");
+
+    }
 
 
     //calc BMI first activity then pass to second the result
     //bmi = weight /(height * height);
     public void calcBMI(View v) {
-        String heightS = height.getText().toString();
-        String weightS = weight.getText().toString();
+        String heightS = heightSpinnerValue;
+        String weightS = weightSpinnerValue;
+
 
         if (heightS != null && !"".equals(heightS) && weightS != null  &&  !"".equals(weightS)) {
 
@@ -83,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
     public void showDetailsBMI(float bmi, String bmiCategory){
         //pass calculated bmi and label to detail activity plus name and gender
         String nameS = name.getText().toString();
-        String genderS = gender.getText().toString();
+        String genderS = genderSpinnerValue;
 
         Intent i = new Intent(MainActivity.this, DetailsBmiActivity.class);
 
